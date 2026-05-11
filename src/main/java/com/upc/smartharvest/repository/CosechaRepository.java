328605561d8d96366655bcd241807ce017f93742
IngridYH20 <ingridyshi@gmail.com>
2026-05-10 23:18:36 -0500

    Update CosechaRepository

--- src/main/java/com/upc/smartharvest/repository/CosechaRepository.java
+++ src/main/java/com/upc/smartharvest/repository/CosechaRepository.java
@@ -1,7 +1,9 @@
 package com.upc.smartharvest.repository;
 
+import com.upc.smartharvest.DTOS.ReporteCosechaDTO;
 import com.upc.smartharvest.entities.Cosecha;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -17,4 +19,15 @@ public interface CosechaRepository extends JpaRepository<Cosecha, Long> {
     List<Cosecha> findByFechaCosechaBetween(LocalDate inicio, LocalDate fin);
 
     List<Cosecha> findByCultivoParcelaId(Long parcelaId);
+
+    @Query(value = "SELECT c.nombre AS nombreCultivo, " +
+            "SUM(co.cantidad_kg) AS totalKG, " +
+            "COUNT(co.id) AS cantidadRegistros " +
+            "FROM cosecha co " +
+            "INNER JOIN cultivo c ON co.cultivo_id = c.id " +
+            "WHERE co.fecha_cosecha BETWEEN ?1 AND ?2 " +
+            "GROUP BY c.nombre " +
+            "ORDER BY totalKg DESC", nativeQuery = true)
+    List<ReporteCosechaDTO> obtenerReporteCosecha(LocalDate fechaInicio, LocalDate fechaFin);
+
 }
